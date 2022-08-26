ba8c32b856b9c270730a359ee20844ff88a8c2a1
xiaojun1111 <112144065+xiaojun1111@users.noreply.github.com>
2022-08-26 17:57:06 +0800

    Update Test.java

--- test/Test.java
+++ test/Test.java
@@ -1,5 +1,7 @@
  public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "肖俊是大哥" );
+        System.out.println( "李腥是条狗" );
+  
     }
 }
